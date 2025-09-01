dcfb1eda0a8e7ef2e6a93040880a8722098adf55
esp32miner <nanartg123@gmail.com>
2025-09-02 04:03:05 +0700

    Update ccmining.sh

--- ccmining.sh
+++ ccmining.sh
@@ -1,27 +1,10 @@
-#!/bin/bash
-# Auto CCminer Termux Script
-
-# Cập nhật gói
+~/ccminer/start.sh
 yes | pkg update && pkg upgrade
-
-# Cài các gói cần thiết
 yes | pkg install libjansson build-essential clang binutils git
-
-# Copy sysctl.h nếu thiếu
-cp /data/data/com.termux/files/usr/include/linux/sysctl.h /data/data/com.termux/files/usr/include/sys 2>/dev/null || true
-
-# Di chuyển về home
-cd $HOME
-
-# Clone ccminer
+cp /data/data/com.termux/files/usr/include/linux/sysctl.h /data/data/com.termux/files/usr/include/sys
 git clone https://github.com/Darktron/ccminer.git
 cd ccminer
-
-# Cấp quyền thực thi cho các script
 chmod +x build.sh configure.sh autogen.sh start.sh
-
-# Biên dịch
 CXX=clang++ CC=clang ./build.sh
-
-# Chạy ccminer
-./start.sh
+nano config.json
+~/ccminer/start.sh
